5aaa4edfd19f3de76a497b02fe6719f2442f2ca3
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-12 12:04:32 -0800

    Finished the header

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/About.scss';
+
+function About() {
+  return (
+    <section className="section-about">
+
+    </section>
+  );
+}
+
+export default About;
