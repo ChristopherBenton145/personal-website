ff2ac55a0fb297b1ce89a1b90245a58da14cb5ae
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-13 16:02:24 -0700

    Updated the readme file

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-This is the start of my personal website project.
+# Personal Website
+
+This is my personal website I created to showcase my talent in programming.
+
+## Built With
+
+- HTML
+- Sass
+- JavaScript
+- React
+
+## Getting Started
+
+1. First you what to install the dependencies so run `npm install` in the project directory.
+2. Once you have all of the dependencies, run `npm run start' in the project directory.
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,18 +1,18 @@
-import React, {useCallback} from 'react';
+import React from 'react';
 import '../styles/Footer.scss';
-import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
+// import confetti from "https://cdn.skypack.dev/canvas-confetti@1";
 
 function Footer() {
-  const pressedMe = useCallback(() => {
-    confetti({
-      particleCount: 300,
-      spread: 100
-    });
-  }, []);
+  // function pressedMe() {
+  //   confetti({
+  //     particleCount: 300,
+  //     spread: 100
+  //   });
+  // }
 
   return (
     <section className="section-footer">
-        <button onClick={pressedMe}>🎉 Press me</button>
+        {/* <button onClick={pressedMe}>🎉 Press me</button> */}
     </section>
   );
 }
