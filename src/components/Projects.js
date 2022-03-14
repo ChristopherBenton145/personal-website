ef961fde409bc46a25cfe48401ee1c99cb9ae1f3
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-14 13:22:50 -0700

    Added my very first website I've ever made to my old projects. And I can say I think I learned a little bit...

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,9 @@ function Projects() {
           <a href="https://multistep-form8276d2.netlify.app/" target="_blank"><button className="button-live">Live</button></a>
           <a href="https://github.com/TempUsername2436/multistep-form" target="_blank"><button className="button-code">Code</button></a>
         </div>
+      </div>
+      <h2>Old Projects</h2>
+      <div className="container">
         <div>
           <div className="image-1"></div>
           <h4>Forms Example</h4>
@@ -29,6 +32,12 @@ function Projects() {
           <h4>Drawing Project</h4>
           <a href="https://drawing-project24665.netlify.app" target="_blank"><button className="button-live">Live</button></a>
           <a href="https://github.com/TempUsername2436/drawing-project" target="_blank"><button className="button-code">Code</button></a>
+        </div>
+        <div>
+          <div className="image-5"></div>
+          <h4>My Very First Website</h4>
+          <h5>This is only here so you can see how far I have come.</h5>
+          <a href="https://myveryfirstwebsite432.netlify.app" target="_blank"><button className="button-live">Live</button></a>
         </div>
       </div>
     </section>
