130e8d3430ed1303bb808a7983f27b86006a7386
TempUsername2436 <christopherpokemon30@gmail.com>
2022-03-16 15:23:26 -0700

    Removed old projects and added a new project. It's an epic space landing page.

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -7,37 +7,16 @@ function Projects() {
       <h2>Projects</h2>
       <div className="container">
         <div>
-          <div className="image-4"></div>
+          <div className="image-2"></div>
+          <h4>Space Landing Page</h4>
+          <a href="https://space-landing-page42.netlify.app/" target="_blank"><button className="button-live">Live</button></a>
+          <a href="https://github.com/TempUsername2436/space-landing-page" target="_blank"><button className="button-code">Code</button></a>
+        </div>
+        <div>
+          <div className="image-1"></div>
           <h4>Multistep Form</h4>
           <a href="https://multistep-form8276d2.netlify.app/" target="_blank"><button className="button-live">Live</button></a>
           <a href="https://github.com/TempUsername2436/multistep-form" target="_blank"><button className="button-code">Code</button></a>
-        </div>
-      </div>
-      <h2>Old Projects</h2>
-      <div className="container">
-        <div>
-          <div className="image-1"></div>
-          <h4>Forms Example</h4>
-          <a href="https://forms-example81c88.netlify.app" target="_blank"><button className="button-live">Live</button></a>
-          <a href="https://github.com/TempUsername2436/forms-exmaple" target="_blank"><button className="button-code">Code</button></a>
-        </div>
-        <div>
-          <div className="image-2"></div>
-          <h4>Landing Page</h4>
-          <a href="https://landing-page992213.netlify.app" target="_blank"><button className="button-live">Live</button></a>
-          <a href="https://github.com/TempUsername2436/landing-page" target="_blank"><button className="button-code">Code</button></a>
-        </div>
-        <div>
-          <div className="image-3"></div>
-          <h4>Drawing Project</h4>
-          <a href="https://drawing-project24665.netlify.app" target="_blank"><button className="button-live">Live</button></a>
-          <a href="https://github.com/TempUsername2436/drawing-project" target="_blank"><button className="button-code">Code</button></a>
-        </div>
-        <div>
-          <div className="image-5"></div>
-          <h4>My Very First Website</h4>
-          <h5>This is only here so you can see how far I have come.</h5>
-          <a href="https://myveryfirstwebsite432.netlify.app" target="_blank"><button className="button-live">Live</button></a>
         </div>
       </div>
     </section>
