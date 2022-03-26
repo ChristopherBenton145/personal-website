a674c99a918ac7d18d82c06ab603ae35bb6eadbe
TempUsername2436 <c.benton.145@gmail.com>
2022-03-26 15:33:58 -0700

    Fixed some links

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -9,10 +9,10 @@ function Contact() {
         <a href="https://www.linkedin.com/in/christopher-benton-78889822a/" target="_blank">https://www.linkedin.com/in/christopher-benton-78889822a/</a>
         <div className="stop"></div>
         <h3>Github</h3>
-        <a href="https://github.com/TempUsername2436" target="_blank">https://github.com/TempUsername2436</a>
+        <a href="https://github.com/ChristopherBenton145" target="_blank">https://github.com/ChristopherBenton145</a>
         <div className="stop"></div>
         <h3>Stack Overflow</h3>
-        <a href="https://stackoverflow.com/users/17382115/temp1234" target="_blank">https://stackoverflow.com/users/17382115/temp1234</a>
+        <a href="https://stackoverflow.com/users/17382115/temp?tab=profile" target="_blank">https://stackoverflow.com/users/17382115/temp?tab=profile</a>
         <div className="stop"></div>
         <h3>Email</h3>
         <a href="mailto: c.benton.145@gmail.com">c.benton.145@gmail.com</a>
